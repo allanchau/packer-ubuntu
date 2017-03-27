a541c15dcf83d02b3867662407ae2f0e3dc64165
Allan Chau <allan.chau@icloud.com>
2017-03-28 10:25:35 +1030

    Merge base box

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v3.0.0 - 28 March 2017
+
+- Add back support for VMWare.
+- Merge the base box into this repo.
+- Add Yarn.
+
 ## v2.0.0 - 22 March 2017
 
 - Add docker to sudoers.
--- scripts/node.sh
+++ scripts/node.sh
@@ -3,3 +3,7 @@
 # Install node.
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# Install Yarn.
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
